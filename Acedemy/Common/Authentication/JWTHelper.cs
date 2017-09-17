657644c1ac47e8737c90afecc98e19119daca362
Christian Pace <pace1987@live.com>
2017-09-17 18:43:51 +0200

    Project Skeleton

---
+++ Acedemy/Common/Authentication/JWTHelper.cs
@@ -0,0 +1,29 @@
+﻿using Jose;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Authentication
+{
+    public static class JWTHelper
+    {
+        private static byte[] GetByteSecret(string secret)
+        {
+            return Encoding.ASCII.GetBytes(secret);
+        }
+
+        public static string CreateSignedToken(Dictionary<string,object> payload, string secretKey)
+        {
+            var secretBytes = GetByteSecret(secretKey);
+
+            return JWT.Encode(payload, secretBytes, JwsAlgorithm.ES512);
+        }
+
+        public static string DecodeSignedToken(string signedToken, string secretKey)
+        {
+            var secretBytes = GetByteSecret(secretKey);
+
+            return JWT.Decode(signedToken, secretBytes);
+        }
+    }
+}
