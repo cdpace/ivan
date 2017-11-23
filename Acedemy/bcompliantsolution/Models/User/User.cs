98c3e0475b8409a48a5c5f75d37b9b70d1a5e2cc
Christian Pace <christianpace105@gmail.com>
2017-11-23 17:16:22 +0100

    Started project skeleton using razer pages

---
+++ Acedemy/bcompliantsolution/Models/User/User.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace Models.User
+{
+    public class User
+    {
+        public User()
+        {
+        }
+
+        //Authentication Details
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+
+        //Personal Details
+        public string IdCardNumber { get; set; }
+        public string Title { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string Town { get; set; }
+        public string PostCode { get; set; }
+        public string Country { get; set; }
+
+        public ushort PhoneCountryCode { get; set; }
+        public int PhoneNumber { get; set; }
+
+        public ushort MobileCountryCode { get; set; }
+        public int MobileNumber { get; set; }
+
+        public string VatNumber { get; set; }
+
+        public bool AreYouAMember { get; set; }
+
+        //Declaration Fiekds
+        public bool TermsAndConditions { get; set; }
+        public bool DataProtectionAct { get; set; }
+
+    }
+}
